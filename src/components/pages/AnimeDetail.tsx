3d5b0cf2c98f3780597574ed7c7667104bfaaa4f
Jakub Gencer <jakub.gencer@allaboutapps.at>
2019-09-03 13:36:51 +0200

    Added a Anime Detail site, new Layout

--- src/components/pages/AnimeDetail.tsx
+++ src/components/pages/AnimeDetail.tsx
@@ -1,17 +1,56 @@
-import React, {Component} from 'react';
-import { withStyles, WithStyles, createStyles } from '@material-ui/styles';
-import { Theme, Button } from '@material-ui/core';
-import { AnimeDetails } from './Anime';
+import { Theme } from '@material-ui/core';
+import { createStyles, withStyles, WithStyles } from '@material-ui/styles';
+import React, { Component } from 'react';
 import { animeModel } from '../../models/animeModel';
 
 const style = (theme: Theme) => createStyles( {
+    animeDetailsMain: {
+        display: 'flex',
+        flexDirection: 'column',
+        margin: '90px 0 0 0',
+        color: 'white',
+    },
+    animeDetailsHeader: {
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'center',
+        fontSize: '40px',
 
-
-
+    },
+    headerImage: {
+        float: 'left',
+        margin: '20px',
+        maxWidth: '15%',
+    },
+    headerTitle: {},
+    animeDetailsContent: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    contentGenres: {
+        float: 'left',
+        margin: '20px',
+        maxWidth: '15%',
+    },
+    contentGenresTitle: {
+        float: 'left',
+        margin: '20px',
+        maxWidth: '15%',
+        textDecoration: 'underline',
+        fontStyle: 'oblique',
+        fontFamily: 'Baloo'
+    },
+    contentGenresDiv: {
+        width: '90%',
+    },
+    contentText: {
+        maxWidth: '90%' 
+    }
 });
 
 interface Props extends WithStyles<typeof style>{
-
+    history: any;
 }
 
 interface State {
@@ -20,17 +59,40 @@ interface State {
 
 class AnimeDetail extends Component<Props, State> {
 
-    
+    componentDidMount() {
+        if (!animeModel.currentSelectedAnime) {
+            this.props.history.push("/anime");
+        }
+    }
+
     render() {
         if (!animeModel.currentSelectedAnime) {
-            return (<div>Kein anime ausgwählt</div>);
+            return (<div className={this.props.classes.animeDetailsMain}>Kein anime ausgwählt</div>);
         }
-
-        console.log("selected", animeModel.currentSelectedAnime.title);
-
+        const anime = animeModel.currentSelectedAnime;
+        console.log(anime.genres[0].name)
         return (
-            <div>
-                
+            <div className={this.props.classes.animeDetailsMain}>
+                <div className={this.props.classes.animeDetailsHeader}>
+
+                    <div className={this.props.classes.headerTitle}>
+                        <img className={this.props.classes.headerImage} src={anime.image_url} alt={`${anime.title}-image`}/>
+                        <p>{anime.title}</p>
+                    </div>
+
+                </div>
+                <div className={this.props.classes.animeDetailsContent}>
+                    
+                    <div className={this.props.classes.contentGenresDiv}>
+                        <p className={this.props.classes.contentGenresTitle}>Genres: </p>
+                        {anime.genres.map( (g) =>  {
+                            return (
+                                <p className={this.props.classes.contentGenres}>{g.name}</p>
+                            );
+                        })}    
+                    </div>
+                    <p className={this.props.classes.contentText}>{anime.synopsis}</p>
+                </div>
             </div>
         );
     }
